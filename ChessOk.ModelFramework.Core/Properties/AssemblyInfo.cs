85ffdaf08ecfd4ec20211ce30ca2742ca126186a
odinserj <odinserj@chessok.com>
2012-10-23 18:33:04 +0400

    Changed assembly title & product

--- ChessOk.ModelFramework.Core/Properties/AssemblyInfo.cs
+++ ChessOk.ModelFramework.Core/Properties/AssemblyInfo.cs
@@ -2,16 +2,16 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ChessOk.ModelFramework.Core")]
+[assembly: AssemblyTitle("ModelFramework.Core")]
 [assembly: AssemblyDescription("Core components for ModelFramework: application bus, validation context, sync and async commands")]
 [assembly: AssemblyCompany("Convekta Ltd.")]
-[assembly: AssemblyProduct("ChessOk.ModelFramework.Core")]
+[assembly: AssemblyProduct("ModelFramework.Core")]
 [assembly: AssemblyCopyright("Copyright © Convekta Ltd. 2011, 2012")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("0f9cc450-a3ba-4cb0-aa39-6967669c710d")]
 [assembly: AssemblyVersion("3.0.0.*")]
-[assembly: AssemblyInformationalVersion("3.0.0-alpha2")]
+[assembly: AssemblyInformationalVersion("3.0.0-alpha3")]
 
 [assembly: InternalsVisibleTo("ChessOk.ModelFramework.Core.Tests")]
